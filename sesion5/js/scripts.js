1bef6b3eea0af5fc672703984003e36815759e88
Criszamora11 <cristobal.zamora@usm.cl>
2024-04-15 01:27:18 -0400

    MI Primer trabajo

---
+++ sesion5/js/scripts.js
@@ -0,0 +1,14 @@
+/*alert("hola mundo")
+*/
+function saludar(){
+    alert("full box")
+}
+/*
+function colorer(){
+  //  console.log("Si")
+    var h1 = document.getElementById("saludo")
+
+    h1.innerHTML = "javascript!"
+
+    h1.classList.add("fondo")
+}
